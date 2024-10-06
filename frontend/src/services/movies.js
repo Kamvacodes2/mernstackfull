8564cdc0865e3e044f924dfed0b3bcb2632cc067
Kamvacodes2 <154967572+Kamvacodes2@users.noreply.github.com>
2024-10-06 20:05:03 +0200

    Update movies.js

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 class MovieDataService {
     getAll(page = 0) {
-        return axios.get(`https://movie-review-backend-o40a.onrender.com/api/v1/movies?page=${page}`)
+        return axios.get(`https://mernstackfull.vercel.app/api/v1/movies?page=${page}`)
     }
     get(id) {
-        return axios.get(`https://movie-review-backend-o40a.onrender.com/api/v1/movies/id/${id}`)
+        return axios.get(`https://mernstackfull.vercel.app/api/v1/movies/id/${id}`)
     }
     find(query, by = "title", page = 0) {
         return axios.get(
-            `https://movie-review-backend-o40a.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
+            `https://mernstackfull.vercel.app/api/v1/movies?${by}=${query}&page=${page}`
         )
     }
     createReview(data) {
-        return axios.post("https://movie-review-backend-o40a.onrender.com/api/v1/movies/review", data)
+        return axios.post("https://mernstackfull.vercel.app/api/v1/movies/review", data)
     }
     updateReview(data) {
-        return axios.put("https://movie-review-backend-o40a.onrender.com/api/v1/movies/review", data)
+        return axios.put("https://mernstackfull.vercel.app/api/v1/movies/review", data)
     }
     deleteReview(id, userId) {
         return axios.delete(
-            "https://movie-review-backend-o40a.onrender.com/api/v1/movies/review",
+            "https://mernstackfull.vercel.app/api/v1/movies/review",
             { data: { review_id: id, user_id: userId } }
         )
     }
